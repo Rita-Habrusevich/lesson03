a231ff3f35059df2d8d6b739c083a96ca92b7bcc
Rita Habrusevich <rita.habrusevich@gmail.com>
2022-06-01 16:35:21 +0300

    create thirdFolder and copy file

--- node.js
+++ node.js
@@ -3,3 +3,5 @@ fse.ensureDirSync('./firstFolder/');
 fse.ensureFileSync('./firstFolder/file.txt');
 fse.ensureDirSync('./secondFolder/');
 fse.moveSync('./firstFolder/file.txt', './secondFolder/file.txt');
+fse.ensureDirSync('./thirdFolder/');
+fse.copySync('./secondFolder/file.txt', './thirdFolder/file.txt');
