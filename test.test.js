a11a49508a717c1aaf11d5e6f5cce8141a8227ae
Rita Habrusevich <rita.habrusevich@gmail.com>
2022-08-18 12:11:44 +0300

    lesson26.2

--- test.test.js
+++ test.test.js
@@ -5,25 +5,48 @@ const cardValidatorSchema = require('./data/cardValidator-v1');
 const validator = new Validator();
 
 
-describe(`API card validator tests`, function() {
+describe(`API air quality tests`, function() {
     test(`should status code equal 200`, async () => {
-        const response = await axios.get('https://card-validator.p.rapidapi.com/generate?cardLength=4', {
+        const response = await axios.get('https://air-quality.p.rapidapi.com/forecast/airquality?lat=35.779&lon=-78.638&hours=72', {
             headers: {
 
                 'X-RapidAPI-Key': '5b9b99e5admsh5b365e0f0f7bf70p1ba55bjsnb09907079119',
-                'X-RapidAPI-Host': 'card-validator.p.rapidapi.com'
+                'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
             }
         });
         await expect(response.status).toEqual(200)
     });
 
+    test(`should status code equal 200`, async () => {
+        const response = await axios.get('https://air-quality.p.rapidapi.com/current/airquality?lon=-73.00597&lat=40.71427', {
+            headers: {
+
+                'X-RapidAPI-Key': '5b9b99e5admsh5b365e0f0f7bf70p1ba55bjsnb09907079119',
+                'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
+            }
+        });
+        await expect(response.status).toEqual(200)
+    });
+
+    test(`should status code equal 200`, async () => {
+        const response = await axios.get('https://air-quality.p.rapidapi.com/current/airquality?lon=-78.638&lat=35.779', {
+            headers: {
+
+                'X-RapidAPI-Key': '5b9b99e5admsh5b365e0f0f7bf70p1ba55bjsnb09907079119',
+                'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
+            }
+        });
+        await expect(response.status).toEqual(200)
+    });
+
+
     test(`should be appropriate json schema`, async () => {
         const response = await axios( {
             method: 'get',
-            url: 'https://card-validator.p.rapidapi.com/generate?cardLength=4',
+            url: 'https://air-quality.p.rapidapi.com/forecast/airquality?lat=35.779&lon=-78.638&hours=72',
             headers: {
                 'X-RapidAPI-Key': '5b9b99e5admsh5b365e0f0f7bf70p1ba55bjsnb09907079119',
-                'X-RapidAPI-Host': 'card-validator.p.rapidapi.com'
+                'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
             }
         });
         let validationResult = await validator.validate(response.data, cardValidatorSchema);
@@ -33,10 +56,10 @@ describe(`API card validator tests`, function() {
     let resp;
     test(`send http request method test`, async() => {
         const config = {
-            url: 'https://card-validator.p.rapidapi.com/generate?cardLength=4',
+            url: 'https://air-quality.p.rapidapi.com/forecast/airquality?lat=35.779&lon=-78.638&hours=72',
             headers: {
                 'X-RapidAPI-Key': '5b9b99e5admsh5b365e0f0f7bf70p1ba55bjsnb09907079119',
-                'X-RapidAPI-Host': 'card-validator.p.rapidapi.com'
+                'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
             }
         };
         resp = await sendHttpRequest(config);
